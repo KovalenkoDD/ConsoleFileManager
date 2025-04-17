3b98bb27aa0005b912c6ee50b106d86d4b4a8965
KovDan <koval_dan2004@mail.ru>
2025-04-17 21:26:45 +0700

    cat реализована

---
+++ cat/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        CatCommand(args);
+    }
+    public static void CatCommand(string[] args)
+    {
+        if (args[0] == "-h" || args[0] == "-help")
+        {
+            Console.WriteLine("Справка: Эта команда выводит содержимое файла.\n" +
+                              "cat <file> - показать содержимое файла");
+            return;
+        }
+        if (args.Length < 1)
+        {
+            Console.WriteLine("Не указан файл для просмотра");
+            return;
+        }
+
+        string filePath = Path.Combine(Directory.GetCurrentDirectory(), args[0]);
+
+        try
+        {
+            Console.WriteLine($"Содержимое файла {filePath}:");
+            Console.WriteLine("----------------------------------------");
+            using (var reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            Console.WriteLine("----------------------------------------");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+}
