896ed54dd0d8082733691c6f861a97d124cdefa1
KovDan <koval_dan2004@mail.ru>
2025-04-17 20:42:30 +0700

    pwd реализована

--- pwd/Program.cs
+++ pwd/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length == 0)
+            PwdCommand();
+        else if (args.Length == 1 && (args[0] == "--help" || args[0] == "-h"))
+            Console.WriteLine("Справка: Эта команда выводит текущую директорию.");
+        else
+            Console.WriteLine("Команда pwd без параметров!");
+    }
+    public static void PwdCommand()
+    {
+        Console.WriteLine($"Текущая директория: {Directory.GetCurrentDirectory()}");
+    }
+}
